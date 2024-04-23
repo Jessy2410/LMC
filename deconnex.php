fe57ddabf0d6f3180a1cc26c7c8db53b791da9f2
Romu Trenou <trenouromuald@gmail.com>
2024-04-23 09:20:57 +0200

    Update deconnex.php

--- deconnex.php
+++ deconnex.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
 session_unset(); // Supprime les données de session
-session_destroy(); // Détruire la session
+session_destroy(); // Détruit la session
 header('Location: pageConnexion.php'); // Redirection après déconnexion
 ?>
